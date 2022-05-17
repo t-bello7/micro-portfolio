b9c168794b869edd2c64c6ab2bf5894a7db438e2
godfrey <godiarksel@gmail.com>
2022-05-17 18:09:21 +0300

    Configure modal HTML skeleton

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,33 +13,41 @@ const works = [
     },
     {
         id: 002,
+        img_dir: `/assets/img/modal-image.png`,
         header: `Profesional Art Printing Data`,
         description: ` A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard`,
         tags: ['html', 'bootstrap', 'Ruby'],
+         content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`
     },
     {
         id: 003,
+        img_dir: `/assets/img/project-img.png`,
         header: `Profesional Art Printing Data`,
         description: ` A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard`,
         tags: ['html', 'bootstrap', 'Ruby'],
+         content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`
 
     },
     {
         id: 004,
+        img_dir: `/assets/img/project-img.png`,
         header: `Profesional Art Printing Data`,
         description: ` A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard`,
         tags: ['html', 'bootstrap', 'Ruby'],
+         content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`
 
     },
     {
         id: 005,
+        img_dir:`/assets/img/project-img.png` ,
         header: `Profesional Art Printing Data`,
         description: ` A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard`,
         tags: ['html', 'bootstrap', 'Ruby'],
+         content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`
 
     }
 ]
@@ -67,6 +75,28 @@ let renderDocuments = (arr, container) => {
             <li><a href="https://www.ruby-lang.org/en/">  ${element.tags[2]} </a></li>
           </ul>
           <button class="btn btn-project" type="button">See Project</button>
+        </div>
+        <div class = "modal"> 
+        <div class = "modal-header">
+        <h3 class="mg-1 ff-crete">${element.header}</h3>
+        <img src="./assets/img/close-icon.png" alt="close-icon">
+        </div>
+        
+        <ul class="mg-1 mg-inline flex tag tag-project">
+          <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"> ${element.tags[0]} </a></li>
+          <li><a href="https://getbootstrap.com/">  ${element.tags[1]} </a></li>
+          <li><a href="https://www.ruby-lang.org/en/">  ${element.tags[2]} </a></li>
+      </ul>
+      <img src="${element.img_dir}" alt="modal-image">
+      <p class="ff-inter">
+        ${element.content}
+      </p>
+      <div class = "flex">
+      <button class="btn" type="button">See Live <img src="./assets/img/see-live-icon.png" alt="see-live-icon">
+      </button>
+      <button class="btn" type="button">See Source <img src="./assets/img/github.png" alt="github">
+      </button>
+      </div>
         </div>
         `
     }); 
