4ae40d5c0f99f1004578d139c362d3f2dad92a66
t-bello7 <bello4aus@gmail.com>
2022-05-19 11:44:04 +0100

    refac: add client side validation to contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const openBtn = document.querySelector('#menu-btn');
 const mobileMenu = document.querySelector('#mobile-menu');
 const closeBtn = document.querySelector('#close-btn');
+const contactForm = document.querySelector('#contact-form');
+const errorMsg = document.querySelector('.error-msg');
+const emailInput = document.querySelector('#email');
 
 openBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('visible');
@@ -9,3 +12,19 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('visible');
 });
+
+
+contactForm.addEventListener('submit', e =>{
+
+  let errorMsgs = [];
+  
+  if ( emailInput.value !== emailInput.value.toLowerCase()){
+    errorMsgs.push('Your email should be in lower case')
+  }
+
+  if (errorMsgs.length > 0){
+    e.preventDefault()
+    errorMsg.innerText = errorMsgs.join('')
+  }
+
+})
