b5f90c7e3205c8c5288e21fd46e4af6d4fbce169
t-bello7 <bello4aus@gmail.com>
2023-03-03 20:35:31 +0100

    - refactor font usage to headers for ff-robins and paragraph to ff-crete - reduce tag sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,19 @@ const works = [
     img_dir: './assets/img/space-hub.png',
     header: "Space Travellers' Hub",
     description: "The Space Travellers' Hub consists of Rockets, Missions, and the My Profile section. The application provides commercial and scientific space travel services by allowing users to book rockets and join selected space missions.",
-    tags: ['Bootstrap5', 'ReactJS', 'Redux', 'React-Router'],
+    tags: [{ 
+        name: 'Bootstrap5',
+        link: "https://getbootstrap.com/"
+      }, {
+        name: 'ReactJS',
+        link: ""
+      }, {
+        name: 'Redux',
+        link: ""
+    }, {
+      name: 'React-Router', 
+      link: ""
+    }],
     content: "The Space Travellers' Hub consists of Rockets, Missions, and the My Profile section. The application provides commercial and scientific space travel services by allowing users to book rockets and join selected space missions.",
     liveLink: 'https://super-space-hub.netlify.app/',
     source: 'https://github.com/t-bello7/Micro-Space-Hub',
@@ -30,7 +42,19 @@ const works = [
     img_dir: './assets/img/finconnect-img.png',
     header: 'Finconnect',
     description: 'A listing web application for financial services',
-    tags: ['Django', 'HTML', 'CSS', 'Python'],
+    tags: [{
+      name: 'Django',
+      link: ""
+      }, {
+        name: 'HTML',
+        link: ""
+      }, {
+        name: 'CSS',
+        link: ''
+      }, {
+        name: 'Python',
+        link: ''
+      }],
     content: ' A full-stack web application to list financial services allowing the company to have a better record of financial partners and a platform for customers to easily access these financial services',
     liveLink: 'https://finconnect.herokuapp.com/',
     source: 'https://github.com/t-bello7/finconnect',
@@ -40,7 +64,15 @@ const works = [
     img_dir: './assets/img/todolist-img.png',
     header: 'Minimal Todo List',
     description: 'A minimal todo list application for keeping track of your tasks',
-    tags: ['HTML', 'CSS', 'JavaScript'],
+    tags: [{
+      name: 'HTML',
+      link: ''}, {
+        name: 'CSS',
+        link: '' 
+      }, {
+        name: 'JavaScript',
+        link: ''
+      }],
     content: 'A minimalist Todo list app that\'s everywhere you are. "To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.',
     liveLink: 'https://t-bello7.github.io/micro-todo/',
     source: 'https://github.com/t-bello7/micro-todo',
@@ -50,7 +82,16 @@ const works = [
     img_dir: './assets/img/formula-two.jpeg',
     header: 'Formula One FanPage',
     description: 'A landing page for formula one fans',
-    tags: ['CSS', 'HTML', 'Javascript'],
+    tags: [{
+      name: 'CSS',
+      link: ''
+    }, {
+      name: 'HTML',
+      link: '',
+    }, {
+      name: 'Javascript',
+      link: ''
+    }],
     content: 'A landing page for formula one fans',
     liveLink: 'https://t-bello7.github.io/micro-capstone-1/',
     source: 'https://github.com/t-bello7/micro-capstone-1',
@@ -60,7 +101,16 @@ const works = [
     img_dir: './assets/img/space-img.png',
     header: 'Space travel',
     description: 'A landing page for space travel and tourism.',
-    tags: ['html', 'CSS', 'Javascript'],
+    tags: [{
+      name: 'html',
+      link: ''
+    }, {
+      name: 'CSS',
+      link: ''
+    }, {
+      name: 'Javascript',
+      link: ''
+    }],
     content: 'In a distant future humans would going on tours to the vast space. Ever felt the like travelling space, this is a landing page for space travel and toursim.',
     liveLink: 'https://t-bello7.github.io/space-travel/',
     source: 'https://github.com/t-bello7/space-travel',
@@ -70,7 +120,22 @@ const works = [
     img_dir: '../assets/img/hotel.png',
     header: 'Hotel Booking Application',
     description: 'Hotel is a website for booking hotels.',
-    tags: ['CSS', 'HTML', 'React','JavaScript', 'Ruby', 'Ruby on Rails'],
+    tags: [{
+      name: 'React',
+      link: ""
+    }, {
+      name: 'CSS',
+      link: ""
+    }, {
+      name:'Ruby',
+      link: "https://www.ruby-lang.org/en/"
+      },{
+        name: 'JavaScript'
+        
+      }, {
+        name: 'Ruby on Rails',
+      }
+    ],
     content: ' Hotel rooms are listed as available and can be reserved by users. Admin users are allowed to add and remove hotels from the database developers to build out the User Interface.',
     liveLink: 'https://hotel-frontend-rosy.vercel.app/',
     source: 'https://github.com/t-bello7/hotel-frontend',
@@ -81,7 +146,15 @@ const works = [
     header: 'SendChamp Pricing Page',
     description: 'A clone of the Sendchamp pricing page',
     content: 'SendChamp Pricig Page is a clone of the official SendChamp Pricing Page',
-    tags: ['Typescript','React', 'Chakra UI'],
+    tags: [{
+      name: 'Typescript',
+      link: ''
+    },{
+      name: 'React',
+      link:"" }, {
+        name: 'Chakra UI',
+        link: ''
+      }],
     source: 'https://github.com/t-bello7/sendchamp-page',
     liveLink: 'https://sendchamp-page.vercel.app',
   }
@@ -160,15 +233,15 @@ const renderDocuments = (arr, container) => {
           <div class="featured flex">
             <img  class="featured-img" src="${element.img_dir}" alt="featured-img" />
             <div>
-              <h3 class="mg-1 ff-crete">${element.header}</h3>
-              <p class="ff-inter">
+              <h3 class="mg-1 ff-roboto">${element.header}</h3>
+              <p class="ff-poppins">
                 ${element.description}
               </p>
-              <ul class="tag tag-feature mg-1 ff-inter flex">
-                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS">  ${element.tags[0]}  </a></li>
-                <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics">  ${element.tags[1]}  </a></li>
-                <li><a href="https://getbootstrap.com/">  ${element.tags[2]}  </a></li>
-                <li><a href="https://www.ruby-lang.org/en/"> ${element.tags[3]}  </a></li>
+              <ul class="tag tag-feature mg-1 ff-poppins flex">
+                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS">  ${element.tags[0].name}  </a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics">  ${element.tags[1].name}  </a></li>
+                <li><a href="">  ${element.tags[2].name}  </a></li>
+                <li><a href=""> ${element.tags[3].name}  </a></li>
               </ul>
               <button class="btn open-btn"  data-id=${element.id} type="button">See Project</button>
             </div>
@@ -178,14 +251,14 @@ const renderDocuments = (arr, container) => {
       (
         container.innerHTML += `
         <div style="background-image:url(${element.img_dir})" class=" text-light project">
-          <h2 class="mg-inline bg-text">${element.header}</h2>
-          <p class="mg-1 mg-inline bg-text">
+          <h2 class="mg-inline bg-gray p-1 ff-roboto">${element.header}</h2>
+          <p class="mg-1 mg-inline ff-poppins p-1 bg-gray ">
                 ${element.description}
           </p>
-          <ul class="mg-1 mg-inline flex tag tag-project">
-            <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"> ${element.tags[0]} </a></li>
-            <li><a href="https://getbootstrap.com/">  ${element.tags[1]} </a></li>
-            <li><a href="https://www.ruby-lang.org/en/">  ${element.tags[2]} </a></li>
+          <ul class="mg-1 mg-inline flex ff-poppins tag tag-project p-1 bg-gray">
+            <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"> ${element.tags[0].name} </a></li>
+            <li><a href="https://getbootstrap.com/">  ${element.tags[1].name} </a></li>
+            <li><a href="https://www.ruby-lang.org/en/">  ${element.tags[2].name} </a></li>
           </ul>
           <button class="btn btn-project open-btn" data-id=${element.id} type="button">See Project</button>
         </div>
@@ -215,14 +288,17 @@ window.addEventListener('load', () => {
       </div>
 
       <ul class="mg-2 mg-inline flex tag tag-featured">
-        <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics">${data.tags[0]}</a></li>
-        <li><a href="https://getbootstrap.com/"> ${data.tags[1]}</a></li>
-        <li><a href="https://www.ruby-lang.org/en/"> ${data.tags[2]}</a></li>
+        <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics">${data.tags[0].name}</a></li>
+        <li><a href="https://getbootstrap.com/"> ${data?.tags[1].name}</a></li>
+        <li><a href="https://www.ruby-lang.org/en/"> ${data?.tags[2].name}</a></li>
+        ${data?.tags[3] ? `<li><a href="https://www.ruby-lang.org/en/"> ${data?.tags[3].name} </a></li> `: ""}
+        ${data?.tags[4] ? `<li><a href="https://www.ruby-lang.org/en/"> ${data?.tags[4].name} </a></li> `: ""}
+         ${data.tags[5] ? `<li><a href="https://www.ruby-lang.org/en/"> ${data.tags[5].name} </a></li> `: ""}
       </ul>
       <div class="flex modal-content">
         <img class="mg-2" src="${data.img_dir}" alt="modal-image">
         <div>
-          <p class="ff-inter">${data.content}</p>
+          <p class="ff-poppins">${data.content}</p>
           <div class="buttons mg-2 flex">
             <button class="btn flex btn-modal btn-source" onclick="window.open('${data.liveLink}')">See Live <img src="./assets/img/see-live-icon.png" alt="see-live-icon"> </button>
             <button class="btn flex btn-modal btn-source" onclick="window.open('${data.source}')">See Source <img src="./assets/img/github.png" alt="github"> </button>
